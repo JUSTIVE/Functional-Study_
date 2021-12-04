cb367bb5cefe47f51d5639999efe4b7f640d3f2b
Ben <ben399399@naver.com>
2021-12-04 19:26:09 +0900

    feat: added some pretty functions

--- shoppingMilk/src/shopping/utility/printer.ts
+++ shoppingMilk/src/shopping/utility/printer.ts
@@ -1,12 +1,33 @@
-import { ShoppingResult } from '../shopping';
+import { Items, ItemType } from '../mart'
+import { ShoppingResult } from '../shopping'
+
+type ItemSummary = {
+  [P in ItemType]: number
+}
+
+function itemSummary(items: Items): ItemSummary {
+  return items.reduce((summary, item) => {
+    if (summary[item.name] !== undefined) summary[item.name] += 1
+    else summary[item.name] = 1
+    return summary
+  }, {} as ItemSummary)
+}
+
+function itemListToString(items: Items): string {
+  return Object.entries(itemSummary(items))
+    .map(([itemName, amount]: [string, number]) => {
+      return `${amount} ${itemName}${amount > 1 ? 's' : ''}`
+    })
+    .join(', ')
+}
 
 export function printShoppingResult(sr: ShoppingResult) {
   switch (sr.resultType) {
     case 'Success':
-      console.log(`[ 👍 Success ] I got ${sr.value.map(x => x.name)}`)
-      break;
+      console.log(`[ 👍 Success ] I got ${itemListToString(sr.value)}`)
+      break
     case 'Failure':
       console.log(`[ ❌ Failed  ] ${sr.value}`)
-      break;
+      break
   }
 }
